aa81c81b930d7c2bcd11b61dbaa82d3a3fccde37
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-01-24 22:35:44 +0200

    update to my answer

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,6 +1,6 @@
 -- a script that displays the number of records with id = 89
 -- in the table first_table of the database hbtn_0c_0 in my MySQL server.
 
-SELECT *
+SELECT COUNT (*)
 FROM first_table
-WHERE id = 89
+WHERE id = 89;
