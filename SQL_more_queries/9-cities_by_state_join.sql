ea296ef530fe85af5fbbcd194c73c509dcba76c9
Ameerah AlGohary <149532189+AmiraAlGohary@users.noreply.github.com>
2024-01-25 03:29:50 +0200

    update to my answer

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- a script that lists all cities contained in the database hbtn_0d_usa.
 
-SELECT cities.id, cities.name, states.name,
+SELECT cities.id, cities.name, states.name
 FROM states
 JOIN cities ON states.id = cities.state_id
 
-ORDER BY cities.id
+ORDER BY cities.id;
